89e3468b1a2ad49ce894f8dcb304a4bb5977fa4e
novalure <hello@novalure.eu>
2026-08-23 23:40:42 +0200

    Apply Spanish positioning to route metadata and rendering

--- app/[locale]/[[...slug]]/page.tsx
+++ app/[locale]/[[...slug]]/page.tsx
@@ -1,3 +1,4 @@
+import "@/content/spanish-market-positioning";
 import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { MarketingPage } from "@/components/MarketingPage";
